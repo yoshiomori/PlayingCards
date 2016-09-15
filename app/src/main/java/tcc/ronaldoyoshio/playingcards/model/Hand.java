06d033ebcddd8cafb76f882b695f2786eaeffb05
Yoshio Mori <yoshiomori90@gmail.com>
2016-09-15 14:27:34 -0300

    Removendo redundância da classe Hand.

--- app/src/main/java/tcc/ronaldoyoshio/playingcards/model/Hand.java
+++ app/src/main/java/tcc/ronaldoyoshio/playingcards/model/Hand.java
@@ -1,14 +1,6 @@
 package tcc.ronaldoyoshio.playingcards.model;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Hand extends ArrayList<String> {
-    public Hand() {
-        super();
-    }
-
-    public void shuffle(){
-        Collections.shuffle(this);
-    }
 }
